99a31df95d1226e6adf8b4c2a64e7c3a444590c5
TechUX <connect.world12345@gmail.com>
2023-10-24 20:36:15 +0530

    Day 1 for Replit is completed - Hello World

---
+++ Replit/Day1 Hello World.py
@@ -0,0 +1,13 @@
+print("""
+Devesh Singh
+October 24th 2023
+I am signing up for Replit's 100 days of Python
+challeng!
+I will make sure to spend some time every day coding
+along for, for a minimum of 10 minutes a day.
+I'll be using Replit, an amazing  online IDE so I can do
+this from my phone wherever I happen to be. No excuses
+for not coding from the middle of a field!
+I am feeling 😃
+You can follow my progress at https://github.com/TechUX/100DaysofCode
+""")
