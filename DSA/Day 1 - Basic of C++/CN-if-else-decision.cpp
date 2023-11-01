4bd48dafa928846997f123c4e695ff7b3d350166
TechUX <connect.world12345@gmail.com>
2023-11-02 00:10:41 +0530

    Question 3, String Comprasion Complete

---
+++ DSA/Day 1 - Basic of C++/CN-if-else-decision.cpp
@@ -0,0 +1,14 @@
+// Problem : https://www.codingninjas.com/studio/problems/if-else-decision-making_8357235
+// Language : C++
+// Submission Timestamp : 02 November 2023 | 00:08:37
+
+string compareIfElse(int a, int b) {
+	// Write your code here
+	if (a>b){
+		return "greater" ;
+	} else if (a<b){
+		return "smaller" ;
+	} else {
+		return "equal" ;
+	}
+}
