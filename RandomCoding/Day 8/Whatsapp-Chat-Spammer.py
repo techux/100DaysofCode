1b558c0e4379ee0704bb0e8328f033c90432cc30
TechUX <connect.world12345@gmail.com>
2023-11-09 23:06:16 +0530

    Whatsapp message spammer in Python

---
+++ RandomCoding/Day 8/Whatsapp-Chat-Spammer.py
@@ -0,0 +1,18 @@
+# pip install pyautogui
+
+import pyautogui
+import time
+
+msg = input("Enter the message: ")
+n = input("How many times ?: ")
+
+count = 5
+while(count != 0):
+	print(f"Started in {count} second...")
+	time.sleep(1)
+	count -= 1
+
+print("Fire in the hole!!!")
+
+for i in range(0,int(n)):
+	pyautogui.typewrite(msg + '\n')
