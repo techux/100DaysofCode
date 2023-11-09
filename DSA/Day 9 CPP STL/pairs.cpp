46358057e1097e27156088c7eb38ff2ed847aa3f
TechUX <connect.world12345@gmail.com>
2023-11-09 23:49:24 +0530

    CPP STL Start - pairs

---
+++ DSA/Day 9 CPP STL/pairs.cpp
@@ -0,0 +1,21 @@
+// Programmed By : Devesh Singh
+// Date : 09 November 2023 | 23:34:35
+// Title : CPP STL Pairs
+
+// CPP program to illustrate Pair in STL
+#include <iostream>
+#include <utility>
+using namespace std;
+
+int main()
+{
+	pair<int, int> number;
+
+	number.first = 100;
+	number.second = 69 ;
+
+	cout << number.first << endl ;
+	cout << number.second << endl;
+
+	return 0;
+}
