53b25b0a556c6bcde0aef96cbf245f97b46fcf7e
TechUX <connect.world12345@gmail.com>
2023-11-10 13:02:47 +0530

    learn more about pairs with different data type

--- DSA/Day 9 CPP STL/pairs.cpp
+++ DSA/Day 9 CPP STL/pairs.cpp
@@ -9,13 +9,41 @@ using namespace std;
 
 int main()
 {
+	// Pair of Integers
 	pair<int, int> number;
-
 	number.first = 100;
 	number.second = 69 ;
-
 	cout << number.first << endl ;
 	cout << number.second << endl;
 
-	return 0;
+	// Pair of characters
+	pair<char,char> character ;
+	character.first = 'D' ;
+	character.second = 'R' ;
+	cout << character.first << endl ;
+	cout << character.second << endl;
+
+	// Pair of Strings
+	pair<string,string> str ;
+	str.first = "Devesh" ;
+	str.second = "Roshni" ;
+	cout << str.first << endl ;
+	cout << str.second << endl;
+
+	// Pair of number and character
+	pair<char,int> info ;
+	info.first = 'Y' ;
+	info.second = 1 ;
+	cout << info.first << endl ;
+	cout << info.second << endl;
+
+	// Pair of String and number 
+	pair <string,int> data ;
+	data.first = "Devesh" ;
+	data.second = 8725 ;
+	cout << data.first << endl ;
+	cout << data.second << endl ;
+
+    return 0;
+
 }
